61107aebbd65dafeee22339e01c7589f2e469ac3
Jacob Wong <jacobsunsetbluff@gmail.com>
2025-04-02 20:01:08 -0700

    Small tweaks the endpoint return values

--- backend/services/generate_metric_response.py
+++ backend/services/generate_metric_response.py
@@ -7,13 +7,9 @@ def generate_metric_response(metric_function, start_date, end_date):
     A helper function to process dates and fetch metric data for any metric function.
     """
     try:
-
+        full_date_range = (f"{date_ranges[0][0].isoformat()} to {date_ranges[-1][1].isoformat()}")
         date_ranges = _process_dates(start_date, end_date)
-
-        full_date_range = (
-            f"{date_ranges[0][0].isoformat()} to {date_ranges[-1][1].isoformat()}"
-        )
-
+        
         data = []
         for date_range in date_ranges:
             entries = metric_function(date_range[0], date_range[1])
@@ -21,7 +17,6 @@ def generate_metric_response(metric_function, start_date, end_date):
                 {
                     "dateRange": f"{date_range[0].isoformat()} to {date_range[1].isoformat()}",
                     "entries": entries,
-                    "count": len(entries),
                 }
             )
         return (
